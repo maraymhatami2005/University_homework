e559e43e1a178246628824f32cdd63ed4d7d9da3
Maryam-Hatami <maryhatami2005@gmail.com>
2024-03-19 18:26:45 +0330

    uploading exercise 4

---
+++ Semester2/Advanced_computer_programming/4th.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+// This function takes 'n' as the number of characters used in a text and calculates the cost of the SMS based on the formula below:
+// For the first 20 characters: 100 RIAL
+// For every 5 extra characters: add 40 RIAL
+int cost(int n)
+{
+    float s;
+    if(n<= 0)
+    {
+        cout << "Error.";
+        return(0);
+    }
+
+    s = 100 + ((n-20)/5) * 40;
+
+    if (((n - 20) % 5) != 0)
+        s = s + 40;
+
+    cout << "Cost = " << s << "\n";
+    
+    return(0);
+
+}
+
+int main()
+{
+    int x;
+    cout << "Enter the number of characters used in text: ";
+    cin >> x;
+    cout << cost(x);
+
+}
