71a6ba4cb8649ac017b3a298839b9491c064b5aa
Maryam-Hatami <maryhatami2005@gmail.com>
2024-03-19 19:18:39 +0330

    uploading exercise 5

---
+++ Semester2/Advanced_computer_programming/5th.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    int z;
+    if(a < b)
+    {
+        z = a;
+        a = b;
+        b = z;
+    }
+    while(a!=b)
+    {
+        z = a - b;
+        if (z <= b)
+        {
+            b = z;
+            a = b;
+        }
+        else
+        {
+            a = z;
+            b = b;
+        }
+    }
+    return(a);
+}
+
+    // or
+    //  The BMM function uses the modulo (remainder) method.
+    int BMM(int a, int b)
+    {
+        int z;
+
+        while(b != 0)
+        {
+            z = a;
+            a = b;
+            b = z%b;
+
+        }
+        return(a);
+
+    }
+
+    int main()
+    {
+        int x, y;
+        cout << "Enter 2 numbers:";
+        cin >> x >>y ;
+        cout << GCD(x,y) << "\n" << BMM(x, y);
+    }
