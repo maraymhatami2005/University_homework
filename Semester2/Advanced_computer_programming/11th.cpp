3bdfc9f095b58708acc3e705c351ad499fcb3b16
maryamhatami2005 <maryhatami2005@gmail.com>
2024-03-24 21:33:39 +0330

    updating

---
+++ Semester2/Advanced_computer_programming/11th.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+using namespace std;
+
+// A function that search the array x[] in ascending order
--- Semester2/Advanced_computer_programming/9th.cpp
+++ Semester2/Advanced_computer_programming/9th.cpp
@@ -1,60 +1,59 @@
 #include<iostream>
 using namespace std;
+// Bubble sort 
 
-// A function that finds the index of the minimum element 
-//in a given range [low, up] of an array x[]
-
-
-int Min(float x[], int low, int up){
-    int i, indexMin = low;
-    for(i = low+1; i<= up; i++)
-        if(x[i] < x[indexMin])
-            indexMin = i;
-    
-    return(indexMin);
-}
-
-// A function that sorts the array x[] in ascending order
-
-int Sort(float x[], int n){
-    int i, index;
+int BubbleSort(float x[], int n)
+{
+    int i, j;
     float temp;
+
     for(i=0; i<n; i++)
     {
-        index = Min(x, i, n-1);
 
-        temp = x[i];
-        x[i] = x[index];
-        x[index] = temp;
+        for(j=0; j<n-i-1; j++)
+        {
 
+            if (x[j] < x[j+1])
+            {
+                temp = x[j];
+                x[j]= x[j+1];
+                x[j+1] = temp;
+            }
+        }
+    }
+return 0;
+}
+// or
+
+int BS(float x[], int n) {
+    int i, j;
+    float temp;
+
+    for (i = n - 1; i >= 0; i--) {
+        for (j = 0; j < i; j++) {
+            if (x[j] > x[j + 1]) {
+                temp = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = temp;
+            }
+        }
     }
 return 0;
 }
 
 int main()
 {
+    float x[10];
     int i;
-    float x[50], a[20];
-
-    for(i=0 ; i<10 ; i++)
-         cin >> x[i];
-
+    cout << "Enter 10 numbers:";
     for(i=0; i<10; i++)
-        cin >> a[i];
+        cin >> x[i];
+    BS(x, 10);
 
-    Sort(x,10);
-    Sort(a, 10);
+    cout << "Sorted array: ";
+    for(i = 0; i<10; i++)
+        cout<< x[i] << " ";
 
-    cout << "Sorted array x:";
-    for(i=0; i<10; i++)
-        cout << x[i] << " ";
-
-
-    cout<< "Sorted array a:";
-    for(i=0; i<10; i++)
-        cout << a[i] << " ";
-
-    
     return 0;
-    
 }
+
