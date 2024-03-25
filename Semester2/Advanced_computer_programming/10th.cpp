c42d86e2d8803e7d440e62749af685c799d22d8e
maryamhatami2005 <maryhatami2005@gmail.com>
2024-03-25 13:18:15 +0330

    uploading exercises

--- Semester2/Advanced_computer_programming/10th.cpp
+++ Semester2/Advanced_computer_programming/10th.cpp
@@ -1,2 +1,31 @@
+#include<iostream>
+using namespace std;
+#define t 2
+// A function that linearly searches the array x[] and returns the index if found, or -1 if not found
 
+int search(float x[], int n, float k)
+{
+    int i; 
+    for(i=0; i<n; i++)
+        if (x[i] == k)
+            return i;
 
+    return -1;
+}
+
+int main()
+{
+    float a[10];
+    int p,i;
+    
+    cout << "Enter 10 numbers:";
+    for(i=0; i<10; i++)
+        cin >> a[i];
+
+    p = search(a, 10, t);
+    
+    if(p == -1)
+        cout << "Not found";
+    else
+        cout << "Found in position: " << p;
+}
--- Semester2/Advanced_computer_programming/11th.cpp
+++ Semester2/Advanced_computer_programming/11th.cpp
@@ -1,4 +1,20 @@
 #include<iostream>
 using namespace std;
 
-// A function that search the array x[] in ascending order
+// A function that binary searches the array x[] and returns the index if found, or -1 if not found
+
+int BinarySearch(float x[], int n, float k)
+{
+    int low = 0, up = n-1, middle;
+    while(low <= up)
+    {
+        middle = (low + up) / 2;
+        if (x[middle] == k)
+            return middle;
+        if(x[middle] > k)
+            up = middle -1;
+        else
+            low = middle +1;
+    }
+        return -1;
+}
