b98caed3fa82248493acfaad62d74d815ba21047
maryamhatami2005 <maryhatami2005@gmail.com>
2024-04-28 13:46:45 +0330

    uploading exercises

---
+++ Semester2/Advanced_computer_programming/25th.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+// working with fractions
+
+class ratio
+{
+    public:
+        void assign(int , int);
+        void print();
+        float r;
+
+    private:
+        int num;
+        int den;
+};
+
+void ratio::assign(int s, int m)
+{
+    if (m ==0)
+    {
+        cout << "Not valid, denominator can not be zero." << endl;
+        return ;
+    }
+    
+    num = s;
+    den = m;
+    r = float(s)/m;
+}
+
+void ratio::print()
+{
+    cout << num<< '/' << den << '=' << r;
+}
+
+int main()
+{ 
+    int num, den;
+    ratio x;
+    x.r = 3.75;
+    cout << "Enter two numbers for Numerator and Denominator" << endl;
+    cin >> num >> den;
+
+    x.assign(num,den);
+    x.print();
+}
