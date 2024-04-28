8b29e769b61b76dbc0b8188eb7329c1c90f12394
maryamhatami2005 <maryhatami2005@gmail.com>
2024-04-28 14:56:33 +0330

    uploading exercise 28

---
+++ Semester2/Advanced_computer_programming/28th.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class ratio
+{
+    public:
+        ratio(int s,int m)
+        {
+            num =s;
+            den = m;
+            r = float(s) / m;
+        }
+
+        ratio(int t)
+        {
+            num = t;
+            den = 1;
+            r = t;
+        }
+
+        ratio()
+        {
+            num = 0;
+            den = 1;
+            r = 0;
+        }
+
+        void print();
+    
+    private:
+        int num;
+        int den;
+        float r;
+
+
+};
+
+
+void ratio::print()
+{
+    cout << num<< '/' << den << '=' << r;
+}
+
+int main()
+{
+    ratio x, y(7), z(5,2); 
+    x.print();
+    cout << endl;
+
+    y.print();
+    cout<< endl;
+
+    z.print();
+    cout << endl;
+
+}
