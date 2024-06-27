67fa0fa7da42e1a6efe7dc99e60675b5d885df19
maryamhatami2005 <maryhatami2005@gmail.com>
2024-06-28 00:10:22 +0330

    uploading exercises

---
+++ Semester2/Advanced_computer_programming/44th.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+//redefining operator +
+
+class Ratio{
+    friend Ratio operator+(const Ratio &, const Ratio&);
+
+    public:
+        
+        Ratio(int a = 0, int b = 1): num(a), den(b){cout << "An object born" << endl;}
+
+        ~Ratio(){cout << "An object died" << endl;}
+
+        void Print(){cout << num << "/" << den << endl;}
+
+
+
+    private:
+
+        int num, den;
+
+
+};
+
+Ratio operator+(const Ratio &x, const Ratio &y)
+{
+    Ratio z((x.num*y.den)+ (y.num*x.den), (y.den * x.den));
+    return z;
+}
+
+int main()
+{
+    Ratio x(1,2) , y(2,3);
+    Ratio c;
+    c = x + y;
+    c.Print();
+}
